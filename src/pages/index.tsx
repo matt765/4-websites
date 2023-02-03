9e59f888dbfaff93966b686dd8205c8c4d777f57
matt765 <mateusz.wyrebek@gmail.com>
2023-02-03 13:40:32 +0100

    Remove unnecessary font imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,6 @@ export default function Home() {
           href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500;600;700;800&display=swap"
           rel="stylesheet"
         />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600;700&display=swap"
           rel="stylesheet"
